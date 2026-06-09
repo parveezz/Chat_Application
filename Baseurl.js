01b03859e001aa0c6e53cabfcd6f542495a3ac18
shiak <shaikparveez290@gmail.com>
2026-06-09 10:14:04 +0530

    Set dynamic backend URL for local and production

--- Baseurl.js
+++ Baseurl.js
@@ -1,1 +1,3 @@
-export const BaseUrl = `http://localhost:5000/api/`;
+export const BaseUrl = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+  ? `http://localhost:5000/api/`
+  : `https://chat-backend-r793.onrender.com/api/`;
