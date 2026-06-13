f6044bd0ba8315ac62bb39d479f066555663fe15
shiak <shaikparveez290@gmail.com>
2026-06-13 23:12:11 +0530

    added the local

--- ServerUrl.js
+++ ServerUrl.js
@@ -1,3 +1,5 @@
-export const ServerUrl = "http://localhost:5000";
+const isLocalhost = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
 
-// export const ServerUrl = "https://chat-backend-r793.onrender.com";
+export const ServerUrl = isLocalhost 
+  ? "http://localhost:5000" 
+  : "https://chat-backend-r793.onrender.com";
--- sockets.js
+++ sockets.js
@@ -1,13 +1,12 @@
 import { io } from "socket.io-client";
 
-// export const sockets = io("https://chat-backend-r793.onrender.com", {
-//       autoConnect: false,
-//       auth: {
-//             token: localStorage.getItem("Token"),
-//       },
-// });
+const isLocalhost = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
 
-export const sockets = io("http://localhost:5000", {
+const BACKEND_URL = isLocalhost 
+  ? "http://localhost:5000" 
+  : "https://chat-backend-r793.onrender.com";
+
+export const sockets = io(BACKEND_URL, {
       autoConnect: false,
       auth: {
             token: localStorage.getItem("Token"),
