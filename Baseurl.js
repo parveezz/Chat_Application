83b34d14f80b2073ed4c8daaddac90dff3a1e23c
shiak <shaikparveez290@gmail.com>
2026-06-13 18:16:04 +0530

    added the render

--- Baseurl.js
+++ Baseurl.js
@@ -1,4 +1,3 @@
-export const BaseUrl = `http://localhost:5000/api/`
+// export const BaseUrl = `http://localhost:5000/api/`
 
-
-// export const BaseUrl = `https://chat-backend-r793.onrender.com/api/`
+export const BaseUrl = `https://chat-backend-r793.onrender.com/api/`
--- ServerUrl.js
+++ ServerUrl.js
@@ -1,1 +1,3 @@
-export const ServerUrl = "http://localhost:5000";
+// export const ServerUrl = "http://localhost:5000";
+
+export const ServerUrl = "https://chat-backend-r793.onrender.com";
